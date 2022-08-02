d04820898f58fa6a6d57f5e10073690448e09686
Bogdan <zuyrinbogdank@gmail.com>
2022-08-02 19:44:44 +0300

    adede tasks

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 
 
 
-const person = {
-    name : 'John',
-    age : 23,
-    a:3,
-    city : 'Gomel',
-    isProgrammer : true,
-    stack:['HTML','CSS','JS'],
-    size:{
-        width:23,
-        height:200,
-        weight:100
-    }
-};
+// const person = {
+//     name : 'John',
+//     age : 23,
+//     a:3,
+//     city : 'Gomel',
+//     isProgrammer : true,
+//     stack:['HTML','CSS','JS'],
+//     size:{
+//         width:23,
+//         height:200,
+//         weight:100
+//     }
+// };
 
 
 
@@ -73,6 +73,7 @@ const person = {
 // const arr = [5,6,7,8,9];
 // const [a,b] = arr;
 
+
 // const { 
 //     age: personAge = 100, 
 //     lastName = 'Ivaniv',
@@ -116,7 +117,7 @@ const person = {
 // const person2  = {
 //     ...person,
 //     stack: [...person.stack],
-//     size: {...person.size}
+//     size: {...person.size},
 // };
 
 // console.log(person2.size === person.size);
@@ -125,21 +126,102 @@ const person = {
 
 
 
-const nick = 'bobik';
+// const nick = 'bobik';
 
-const dog = { 
-    age: 2,
-    nick
+// const dog = { 
+//     age: 2,
+//     nick
+// }
+
+// console.log(dog);
+
+
+// const str = 'city';
+
+// const userPerson = { 
+//     name: 'Valerchik',
+//     [srt] : 'Gomel'
+// }
+
+
+
+
+
+
+
+//============================tasks=============================================
+
+// 1. создать объект subscribers с полями oldSubscribers и newSubscribers
+// добавьте в объект поле allSubscribers которое должно
+//  хранить сумму oldSubscribers + newSubscribers
+//  если число allSubscribers > 700 вывести you are famous, иначе try harder
+
+
+// const subscribers = {
+//     oldSubscribers: 300,
+//     newSubscribers: 250,
+// }
+// subscribers.allSubscribers = subscribers['oldSubscribers'] + subscribers['newSubscribers'];
+
+
+// if(subscribers['allSubscribers'] > 700){
+//     console.log('you are famous');
+// }else{
+//     console.log('try harder');
+// }
+
+
+//  2. дан массив объектов [
+//     {a:2, b: 1}, {a:5, b:12}, {a: 95, b: 7}
+//  ]
+//  используя методы массивов и циклы 
+//  вывести в консоль 'its valid', если во всех объектах поле a > b
+
+// let objectArr = [{a:2, b: 1}, {a:5, b:12}, {a: 95, b: 7}];
+// let validCount = 0;
+// for(let i = 0; i < objectArr.length; i++){
+//     if(objectArr[i].a > objectArr[i].b){
+//       validCount++
+//     }
+// }
+
+// if(objectArr.length === validCount){
+//     console.log('its valid');
+// }else{
+//     console.log('no valid');
+// }
+
+
+
+
+// 3. на основе исходного массива создать массив таких же объектов, но поле b
+//  должно быть возведено в квадрат
+//  пример: [{a: 2, b: 1}, {a: 5, b: 144}, {a: 95, b: 49}]
+
+
+let arrOfObj = [{a: 2, b: 1}, {a: 5, b: 144}, {a: 95, b: 49}];
+
+let povArrOfObj = [];
+
+for(let i = 0; i < povArrOfObj.length;i++){
+   
 }
 
-console.log(dog);
+console.log(povArrOfObj);
 
 
-const str = 'city';
 
-const userPerson = { 
-    name: 'Valerchik',
-    [srt] : 'Gomel'
-}
 
+//  4. создать массив на основе предыдущего, в котором
+//  поля a и b будут последовательно помещены в массив
+//  пример: [ 2, 1, 5, 144, 95, 49]
+
+// let arr = [{a: 2, b: 1}, {a: 5, b: 144}, {a: 95, b: 49}];
+// let arr2 = [];
+
+// for(let i = 0; i < arr.length;i++){
+//     arr2.push(arr[i].a, arr[i].b);
+// }
+
+// console.log(arr2);
 

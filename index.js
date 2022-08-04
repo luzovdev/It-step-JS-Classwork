1b00bb96d0c07bcd6307039d442d873ea5eb6b6d
Bogdan <zuyrinbogdank@gmail.com>
2022-08-04 18:41:57 +0300

    added task2 function

--- index.js
+++ index.js
@@ -44,19 +44,34 @@
 //1. Написать функцию которая возвращает среднее арифм 4х чисел
 
 
-function agv (a,b,c,d){
-    return (a + b + c + d) / 4;
-}
+// function agv (a,b,c,d){
+//     return (a + b + c + d) / 4;
+// }
 
-console.log(agv(5,5,523,67));
+// console.log(agv(5,5,523,67));
 
+
+// ДЗ
 //2. Написать функцию которая возвращает среднее арифм любого количества эл-тов чисел
 
-function agv2 (...arrArguments){
-    console.log(arguments);
-    // const arrArguments = [...arguments]; --- это заменятеся в ()
+// function agv2example (...arrArguments){
+//     // const arrArguments = [...arguments]; --- это заменятеся в ()
 
-}
+
+// }
 // arguments --- нету методов массива,  поэтому это не массив, а ПСЕВДОМАССИВ.
-agv2(2,34,5,6,7,8)
+// agv2example(2,34,5,6,7,8)
 
+
+function avg2 (){
+    let arrArguments = [...arguments];
+    let summ = 0;
+    let countOfNums = 0;
+    for(let i = 0; i < arrArguments.length; i++){
+        summ += arrArguments[i];
+        countOfNums++;
+    }
+    return console.log(summ / countOfNums); 
+}
+
+avg2(2,4,5,6);

a814e01c60a9c5be688bba003ef0a68916073182
Bogdan <zuyrinbogdank@gmail.com>
2022-08-04 20:12:04 +0300

    added a new material

--- index.js
+++ index.js
@@ -29,15 +29,95 @@
 // const test = add; // --- тоже function expression
 // console.log(test(2,2));
 
+                                                                //hosting
+//hosting --- всплытие переменных
+//temporary dead zone --- это та часть когда, где всплытие произошло но доступа еще нет.
+
+//error
+
+// console.log(a);
+// let a = 0;
+
+
+// foo();
+// bar(); --- error
+
+// function foo (){
+//     console.log("Iam fooo");
+// }
+
+// const bar = function(){
+//     console.log("Im bar");
+// }
+
+// anonimus self inworking function (IIFE)
+// (function foo (){
+//     console.log("Iam fooo");
+// })()
+
+
+//modules
+
+// let sum = 30;
+// let sumPeople = 20;
+// let sumPeopleSmoking  = 40;
+
+// const result = (function(){
+//     let sumPeople = 99;
+//     return sumPeople
+// })()
 
 
 
+                                //FUNCTION SCOPE BLOCK SCOPE
+
+
+//BLOCK SCOPE
+// let a = 2; 
+// var b = 3;
+
+// if(true){
+//     let a = 4;
+//     var b = 5;
+// }
+
+// console.log(a);
+// console.log(b);
+
+
+//FUNCTION SCOPE
+
+// let a = 2; 
+
+// function foo(){
+//     let a = 3;
+//     console.log(a); 
+//     function bar (){
+//         let a = 4;
+//         console.log(a);
+//     }
+//     bar()
+// }
+// foo()
+
+// console.log(a);
 
 
 
+// let a = 2; 
+
+// function foo(){
+//     let a = 3;
+//     function bar (){
+//         console.log(a);
+//     }
+//     bar()
+// }
+// foo()
 
 
 
+//===========================================tasks======================================
 
 
 
@@ -63,15 +143,13 @@
 // agv2example(2,34,5,6,7,8)
 
 
-function avg2 (){
-    let arrArguments = [...arguments];
-    let summ = 0;
-    let countOfNums = 0;
-    for(let i = 0; i < arrArguments.length; i++){
-        summ += arrArguments[i];
-        countOfNums++;
-    }
-    return console.log(summ / countOfNums); 
-}
+// function avg2 (){
+//     let arrArguments = [...arguments];
+//     let summ = 0;
+//     for(let i = 0; i < arrArguments.length; i++){
+//         summ += arrArguments[i];
+//     }
+//     return console.log(summ / arrArguments.length); 
+// }
 
-avg2(2,4,5,6);
+// avg2(2,4,5,6);

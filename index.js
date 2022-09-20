947904a88dda60dc2d8cde1b8e46e3a2493e56e8
Web <web@it.step>
2022-09-20 21:23:46 +0300

    promises

--- index.js
+++ index.js
@@ -2,3 +2,35 @@
 //promises 
 //async await
 
+
+
+// func(bar(foo())) // callback hell
+
+//функция которая передается в промис выполняется синхронно
+// const pr = new Promise(() =>{
+
+// });
+
+// console.log(pr);
+
+new Promise((resolve,reject) => {
+    resolve('result')
+}).then((res) =>{
+    console.log(res);
+    return res;
+}).then((res) => {
+    console.log(res);
+})
+
+// setTimeout(() => {
+//     console.log('one');
+// }, Math.random() * 1000);
+// setTimeout(() => {
+//     console.log('two');
+// }, Math.random() * 1000);
+// setTimeout(() => {
+//     console.log('three');
+// }, Math.random() * 1000);
+
+
+
